26ebb354b54f1651ab209e32ff84f826e68daf34
halex2005 <halex2005@e1.ru>
2014-09-17 02:58:20 +0600

    added simple thread id test

--- tests/platform/thread_functions.tests.cpp
+++ tests/platform/thread_functions.tests.cpp
@@ -1,2 +1,17 @@
 #include <catch/catch.hpp>
 #include <platform/thread_functions.h>
+#include <thread>
+
+TEST_CASE("get_current_thread_id", "[thread][platform]")
+{
+    auto tid = platform::current_thread_id();
+
+    platform::thread_id_t childTid = 0;
+    std::thread child = std::thread([&childTid]() { childTid = platform::current_thread_id(); });
+    child.join();
+
+    std::cout << "tid      =" << std::hex << tid << std::endl;
+    std::cout << "child tid=" << std::hex << childTid << std::endl;
+
+    REQUIRE(childTid != tid);
+}
