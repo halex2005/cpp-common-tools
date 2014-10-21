a31cacf1bddcbe0d871f88e2f0222778b5a78793
halex2005 <halex2005@e1.ru>
2014-10-21 18:12:32 +0600

    add array_size.h because of common usage with strings

--- strings.h
+++ strings.h
@@ -1,6 +1,7 @@
 #ifndef __STRINGS_HEADER_H__
 #define __STRINGS_HEADER_H__
 
+#include "array_size.h"
 #include "strings/formatter.h"
 #include "strings/string_functions.h"
 #include "strings/string_template.h"
